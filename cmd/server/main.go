e32a140ae5c5118bba9aa83bc8c8ba1fcff50ef7
Mark Leo Cativo <cativomarkleo@gmail.com>
2026-01-05 20:05:59 +0800

    setup amqp

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	_ = conn
+
+	fmt.Println("Successfully connected to RabbitMQ")
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	<-ctx.Done()
+
+	fmt.Println("Shutting down the server...")
 }
